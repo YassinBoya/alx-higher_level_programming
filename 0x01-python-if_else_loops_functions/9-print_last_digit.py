293d97b44ed769f4036715182434962528883b86
Yassine <hondayassin5@gmail.com>
2023-11-28 23:21:44 +0100

    commit of task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        last_digit = number % 10
+        print('{}'.format(last_digit))
+    elif number < 0:
+        last_digit = (number % (-10)) * (-1)
+        print('{}'.format(last_digit))
+    return (last_digit)
