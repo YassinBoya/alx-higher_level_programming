d8c97db04450c46badb2af3ceea284917b9944d7
Yassine <hondayassin5@gmail.com>
2023-11-28 23:53:44 +0100

    commit of task 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
-    power_res = 1
-    if b < 0:
-        a = 1 / a
-        b = abs(b)
-    for _ in range(b):
-        power_res *= a
-    if b < 0:
-        power_res = 1 / power_res
-    return round(power_res, 15)
+    result = 1
+
+    if b >= 0:
+        for _ in range(b):
+            result *= a
+    else:
+        for _ in range(abs(b)):
+            result /= a
+
+    return result
