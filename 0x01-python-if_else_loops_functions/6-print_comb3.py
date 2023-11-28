a366cdabbe5bfa228860474ac50e5409d914467d
Yassine <hondayassin5@gmail.com>
2023-11-28 20:33:53 +0100

    commit of task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(1 + i, 10):
-        print('{}{}'.format (i, j), end=', ' if i < 8 or j <9 else '')
+        if i == 8 and j == 9:
+            print('{}{}'.format(i, j), end='')
+            break
+        print('{}{}'.format(i, j), end=', ')
 print()
