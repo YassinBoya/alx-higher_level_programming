5fea85db1791fc025bc00d25babd573525ecfb3c
Yassine <hondayassin5@gmail.com>
2023-11-28 23:07:52 +0100

    commit of task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    uppercase_str = ""
+    for c in str:
+        if 'a' <= c <= 'z':
+            uppercase_char = chr(ord(c) - ord('a') + ord('A'))
+            uppercase_str += uppercase_char
+        else:
+            uppercase_str += c
+    return uppercase_str
+
