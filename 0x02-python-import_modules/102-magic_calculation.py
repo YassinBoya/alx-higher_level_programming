23054a1da12c693e347a58f08308e17c9c2b5e48
Yassine <hondayassin5@gmail.com>
2023-12-01 00:24:39 +0100

    commit of task 9

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
-
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
@@ -11,4 +9,5 @@ def magic_calculation(a, b):
         return (c)
 
     else:
-        return(sub(a, b))
+        return (sub(a, b))
+    
