7fa5c08e6d9590833e3ca68ce2f2ccdef1eb4746
Yassine <hondayassin5@gmail.com>
2023-12-01 23:48:21 +0100

    commit of task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    index = len(my_list) - 1
+    for _ in my_list:
+        print('{}'.format(my_list[index]))
+        index -= 1
+'''
+def print_reversed_list_integer(my_list=[]):
+    for item in reversed(my_list):
+        print('{}'.format(item))
+'''
