108c900bc73e853bb603e1ce6b2c5f45cb5006be
Yassine <hondayassin5@gmail.com>
2023-12-02 16:34:09 +0100

    commit of task 4

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_str = []
+    for char in my_string:
+        if char != 'C' and char != 'c':
+            new_str.extend(char)
+    res_str = ''.join(new_str)
+    return(res_str)        
