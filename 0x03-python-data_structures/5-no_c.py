7bb5bb2ee62b67aad6f8de837e87704c63157dd6
Yassine <hondayassin5@gmail.com>
2023-12-02 18:20:19 +0100

    commit of task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-'''
 def no_c(my_string):
     new_str = ""
     if my_string:
         for char in my_string:
             if char != 'C' and char != 'c':
                 new_str += char
-    return(new_str)
-'''
-
-
-def no_c(my_string):
-    new_string = my_string.translate({ord(i): None for i in 'cC'})
-    return new_string
+    return (new_str)
