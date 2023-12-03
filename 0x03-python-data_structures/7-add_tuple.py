c96290b8781c8767cbc90167ceaa9f46209ca3da
Yassine <hondayassin5@gmail.com>
2023-12-03 19:51:06 +0100

    commit of task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,4 +6,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_2 = tuple_b + (0, 0)
     for i in range(2):
         sum_elem += (tuple_1[i] + tuple_2[i],)
-    return(sum_elem)
+    return (sum_elem)
