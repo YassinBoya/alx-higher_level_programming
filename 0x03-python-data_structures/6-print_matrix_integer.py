a7b29be9bdee3a5895b9acd740705137f594bb74
Yassine <hondayassin5@gmail.com>
2023-12-03 16:58:14 +0100

    commit of task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    
     if matrix:
         for row in matrix:
             for col_i in row:
-                if col_i != 0:
-                    print(" ", end="")
-                print('{:d}'.format(col_i), end="")
+                space = ""
+                if col_i != row[-1]:
+                    space = " "
+                print('{:d}'.format(col_i), end=space)
             print()
