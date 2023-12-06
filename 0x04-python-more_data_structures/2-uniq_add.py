f44253c31b5cf03b2e8b25961694630a3cf6d9e3
Yassine <hondayassin5@gmail.com>
2023-12-06 23:09:39 +0100

    commit of task 1

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     unique_list = set()
-    unique_list = [num for num in my_list \
-                   if num not in unique_list and not unique_list.add(num)]
+    unique_list = [num for num in my_list if num not in unique_list
+                   and not unique_list.add(num)]
     return sum(unique_list)
