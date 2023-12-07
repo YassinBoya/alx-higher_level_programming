f40d78f138f2a627c8bf0f118e0f4f1ac6473aa7
Yassine <hondayassin5@gmail.com>
2023-12-07 02:29:52 +0100

    commit of task 101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
     squares = list(map(lambda row:
-                    list(map(lambda num: num * num, row)), matrix))
+                       list(map(lambda num: num * num, row)), matrix))
     return (squares)
