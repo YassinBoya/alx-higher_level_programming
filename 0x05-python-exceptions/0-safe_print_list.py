07d7a5fe88d0d321abefeb7e731f450600c89a41
Yassine <hondayassin5@gmail.com>
2023-12-18 22:57:49 +0100

    commit of task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,13 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        if (x > 5):
+        if x > 5:
             x = 5
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-        print()    
     except IndexError:
-        pass
-    return (x)    
+        print("\nIndexError: Provided index is out of range.")
+        x = i
+    finally:
+        print()
+    return x  
