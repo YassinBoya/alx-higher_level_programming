9d6ea10925e193eded8446995db90bab1a2e2cea
Yassine <hondayassin5@gmail.com>
2023-12-18 22:51:37 +0100

    commit of task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        if (x > 5):
+            x = 5
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+        print()    
+    except IndexError:
+        print("\nindex not found")
+    return (x)    
