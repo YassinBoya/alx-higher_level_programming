c5af94f8ceea6237d3c5a1e624bd5ba15b2dc7f8
Yassine <hondayassin5@gmail.com>
2023-12-19 03:03:29 +0100

    commit of task 11

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     try:
         res = fct(args[0], args[1])
-        return (res)
     except Exception as ex:
         print("Exception: {}".format(ex), file=sys.stderr)
-        return (None)
+        res = None
+    return (res)
