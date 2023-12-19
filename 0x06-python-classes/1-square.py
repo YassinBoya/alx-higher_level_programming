e3d20941d1ffcea3bbaee2805292ba6867010149
Yassine <hondayassin5@gmail.com>
2023-12-20 00:42:12 +0100

    commit of task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Define a Square."""
+
+
+class Square:
+    """Represent a Square."""
+
+    def __init__(self, size):
+
+        """Initiaize a new Square.
+
+        Args:
+            size (int): the size of a new square.
+        """
+
+        self.__size = size
