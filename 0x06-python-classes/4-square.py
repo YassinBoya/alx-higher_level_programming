9ea2c3943f383d119821f2e70c07f6728bb07439
Yassine <hondayassin5@gmail.com>
2023-12-20 01:26:57 +0100

    commit of task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""Define a Square."""
+
+
+class Square:
+    """Represent a Square."""
+
+    def __init__(self, size=0):
+
+        """Initiaize a new Square.
+
+        Args:
+            size (int): the size of a new square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """get the square size"""
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Set the square size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+        return (self.__size * self.__size)
