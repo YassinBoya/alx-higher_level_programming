d81f992446eaf6b03b55db60cf5e290f196fe764
Yassine <hondayassin5@gmail.com>
2023-12-20 01:43:26 +0100

    commit of task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+
+"""Define a Square."""
 
 
 class Square:
-    """
-    Class that defines properties of square by: (based on 3-square.py).
+    """Represent a Square.
 
     Attributes:
         size: size of a square (1 side).
     """
+
     def __init__(self, size=0):
-        """Creates new instances of square.
+
+        """Initiaize a new Square.
 
         Args:
-            size: size of the square (1 side).
+            size (int): the size of a new square.
         """
         self.__size = size
 
-    def area(self):
-        """Calculates the area of square.
-
-        Returns: the current square area.
-        """
-        return self.__size ** 2
-
     @property
     def size(self):
-        """Returns the size of a square
-        """
-        return self.__size
+        """get the square size"""
+        return(self.__size)
 
     @size.setter
     def size(self, value):
@@ -47,3 +41,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+        return (self.__size * self.__size)
