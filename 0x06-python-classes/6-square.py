59cf0922c83550c907bf1b21318daef891f1ee90
Yassine <hondayassin5@gmail.com>
2023-12-20 02:24:56 +0100

    commit of task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,18 +28,18 @@ class Square:
     def size(self):
         """get the square size"""
         return (self.__size)
-    
+
     @property
     def position(self):
         """get the square position"""
         return (self.__position)
-    
+
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) 
-            or len(value) != 2 
-            or not all(isinstance(num, int) for num in value) 
-            or not all(num > 0 for num in value)):
+        if (not isinstance(value, tuple)
+                or len(value) != 2
+                or not all(isinstance(num, int) for num in value)
+                or not all(num > 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
