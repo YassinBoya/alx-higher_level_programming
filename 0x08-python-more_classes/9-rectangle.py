1fd0fb6c01b8ef64c9fc669db885f589fe1ee72a
Yassine <hondayassin5@gmail.com>
2024-01-07 01:45:23 +0100

    commit of task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -50,6 +50,10 @@ class Rectangle:
         else:
             self.__height = value
 
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
+
     def area(self):
         """Return the area of a rectangle"""
 
@@ -94,11 +98,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        elif Rectangle.area(rect_1) >= Rectangle.area(rect_2):
+        if rect_1.area() >= rect_2.area():
             return rect_1
         else:
             return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        return cls(size, size)
