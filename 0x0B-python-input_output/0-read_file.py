41ffb177cf2f4b76f9e56fccff49a16caf7a7fd5
Yassine <hondayassin5@gmail.com>
2024-01-10 01:39:40 +0100

    commit of task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    with open(filename, 'r') as file:
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as file:
         print(file.read())
