7fbfbd517194d2e1e79f427a139933a04072aec9
Yassine <hondayassin5@gmail.com>
2024-01-11 01:52:58 +0100

    commit of task 5

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines the BaseGeometry Class"""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    def area(self):
+        raise Exception("area() is not implemented")
