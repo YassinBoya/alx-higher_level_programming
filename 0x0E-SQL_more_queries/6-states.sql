8fe6a77596c736f3889eb26ed208369acbc787e3
Yassine <hondayassin5@gmail.com>
2024-02-14 21:15:35 +0100

    sql task 7

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- a script that creates the database hbtn_0d_usa and the table
---states (in the database hbtn_0d_usa) on your MySQL server
+-- states (in the database hbtn_0d_usa) on your MySQL server
 
 CREATE database IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
