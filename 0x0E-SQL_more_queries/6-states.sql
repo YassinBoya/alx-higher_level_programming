35582ea535cca3b394eb8cbc5af48cdfdbaa7bef
Yassine <hondayassin5@gmail.com>
2024-02-14 21:15:13 +0100

    sql task 7

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,4 +2,5 @@
 --states (in the database hbtn_0d_usa) on your MySQL server
 
 CREATE database IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE table IF NOT EXISTS states(id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
