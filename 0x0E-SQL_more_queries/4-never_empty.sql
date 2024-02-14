47401fe42139b30f990ebd2e6020693cef4bbf55
Yassine <hondayassin5@gmail.com>
2024-02-14 21:09:41 +0100

    sql task 6

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+--  a script that creates the table id_not_null on your MySQL server.
+
+CREATE table IF NOT EXISTS id_not_null(id INT NOT NULL DEFAULT 1, name VARCHAR(256));
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,3 @@
+--  a script that creates the table unique_id on your MySQL server.
+
+CREATE table IF NOT EXISTS unique_id(id INT NOT NULL DEFAULT 1 UNIQUE, name VARCHAR(256));
