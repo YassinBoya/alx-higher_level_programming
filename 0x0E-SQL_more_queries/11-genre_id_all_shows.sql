ccc13d6ae38488b1dc10467cff17fda95dca00da
Yassine <hondayassin5@gmail.com>
2024-02-15 03:00:39 +0100

    sql task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,5 +2,6 @@
 
 SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL')
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
