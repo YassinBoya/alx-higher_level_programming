4b85fb3af3385862c6e5a8e6508be762c5f7b513
Yassine <hondayassin5@gmail.com>
2024-02-15 03:57:11 +0100

    sql task 119

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,18 +1,9 @@
--- script that lists all non-Comedy shows in the database hbtn_0d_tvshows
--- tv_genres table contains only one record where name = Comedy (but the id can be different)
--- each record should display: tv_shows.title
--- results must be sorted in ascending order by the show title
--- can use max two SELECT statements
--- the database name will be passed as an argument of the mysql command
+-- lists all tv shows by their rating
 
-SELECT tv_shows.title
+SELECT tv_shows.title,
+SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
-WHERE tv_shows.title NOT IN (
-SELECT tv_shows.title
-FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = "Comedy")
-ORDER BY tv_shows.title ASC;
+INNER JOIN tv_show_ratings
+ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
