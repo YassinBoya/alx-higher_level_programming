28f279a38e676cd648cc81ca76ebde4e8169dcae
Yassine <hondayassin5@gmail.com>
2024-02-15 03:33:45 +0100

    sql task 14

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- lists all shows in hbtn_0d_tvsows that have at least 1 genre linked
 
-SELECT tv_genres.genre AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(*) AS 'number_of_shows'
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.genre
-HAVING number_of_shows > 0
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
 ORDER BY number_of_shows DESC;
