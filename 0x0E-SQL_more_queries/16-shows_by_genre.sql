7e6ebffc0afc68dae1f71a2cc5c1d2d8c4ad2136
Yassine <hondayassin5@gmail.com>
2024-02-15 03:48:49 +0100

    sql task 14

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,4 +3,9 @@
 -- Each record should display: tv_shows.title - tv_genres.name
 -- Results must be sorted in ascending order by the show title and genre name
 
-SELECT tv_shows.title, tv_genres.name
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name ASC;
