ba77d6f52cc534f680e4e1fecd77f5fbcd200320
Yassine <hondayassin5@gmail.com>
2024-02-15 03:42:08 +0100

    sql task 14

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,7 +3,7 @@
 -- Each record should display: tv_shows.title
 -- Results must be sorted in ascending order by the show title
 
-SELECT tv_shows.title From tv_shows
+SELECT tv_shows.title FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
