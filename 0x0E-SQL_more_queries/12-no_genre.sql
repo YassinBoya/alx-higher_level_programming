a8458edc8beda1005657ef53e82ca93776d4ac3b
Yassine <hondayassin5@gmail.com>
2024-02-15 03:04:59 +0100

    sql task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,1 +1,6 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+from tv_shows
+join tv_show_genres
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
