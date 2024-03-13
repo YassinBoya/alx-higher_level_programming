579dd339a8848d5770e59c03938c90aa20668c06
Yassine <hondayassin5@gmail.com>
2024-03-13 16:17:53 +0000

    task 3 javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,6 @@
 
 const { argv } = require('node:process');
 
-// The 'argv' array always contains at least two elements:
-// [0] path to node, [1] path to the script file.
-// Additional arguments are from index 2 onwards.
-
 if (argv.length === 2) {
   console.log('No argument');
 } else if (argv.length === 3) {
