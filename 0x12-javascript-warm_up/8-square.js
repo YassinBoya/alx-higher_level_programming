13dd01d03d933a53bba09ba4f1c9c2a6f48b5108
Yassine <hondayassin5@gmail.com>
2024-03-13 23:06:59 +0000

    task 8 javascript

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,5 +6,9 @@ if (isNaN(parseInt(argv[2]))) {
   console.log('Missing size');
 }
 for (let i = 0; i < argv[2]; i++) {
-  console.log('x'.repeat(parseInt(argv[2])));
+  let line = '';
+  for (let j = 0; j < argv[2]; j++) {
+    line += 'X';
+  }
+  console.log(line);
 }
