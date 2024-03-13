0b805d24ffa7de402c32648bc1a83a7d494fc6ec
Yassine <hondayassin5@gmail.com>
2024-03-13 22:58:08 +0000

    task 8 javascript

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,5 +6,9 @@ if (isNaN(parseInt(argv[2]))) {
   console.log('Missing number of occurrences');
 }
 for (let i = 0; i < argv[2]; i++) {
-  console.log('C is fun');
+  let line = '';
+  for (let j = 0; j < argv[2]; j++) {
+    line += 'x';
+  }
+  console.log(line);
 }
