de6d8525afbcdcde720a3c0741cd36a53bef507b
Yassine <hondayassin5@gmail.com>
2024-06-19 16:51:59 +0100

    1 task

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Print string “C is fun Python is cool Javascript is amazing”
+
+let firstLine = 'C is fun';
+let secondLine = 'Python is cool';
+let thirdLine = 'JavaScript is amazing';
+
+console.log(`${firstLine}\n${secondLine}\n${thirdLine}`);
