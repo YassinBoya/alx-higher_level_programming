4d6e487b78c4b662ed759e34323b8741aa830b0c
Yassine <hondayassin5@gmail.com>
2024-06-23 18:14:09 +0100

    3 task

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // prints the first argument passed to teh command
 
-if (process.argv[2] == null){
-	console.log('No argument')
-}
-else{
-	console.log(process.argv[2])
+if (process.argv[2] == null) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
