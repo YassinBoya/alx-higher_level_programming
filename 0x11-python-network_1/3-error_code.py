9c076593c4bcd7ff50967c3777961ea518e40842
Yassine <hondayassin5@gmail.com>
2024-07-18 19:45:56 +0100

    3 task

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL 
+Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8).
 """
 
