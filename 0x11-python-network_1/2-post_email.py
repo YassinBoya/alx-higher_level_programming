ee1a2eb824a80824468a3d6b1bfc82e91e176677
Yassine <hondayassin5@gmail.com>
2024-07-18 19:16:18 +0100

    2 task

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Script that takes in a URL and an email, sends a POST request
+ and displays the body of the response
+"""
+
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = {'email': sys.argv[2]}
+    
+    data = urllib.parse.urlencode(data).encode('utf-8')
+    
+    request = urllib.request.Request(url, data)
+    
+    with urllib.request.urlopen(request) as response:
+        response_data = response.read()
+        print(response_data.decode('utf-8'))
