3ee9f24df3b244d1f33cc8ed3a40d060f4f3e4ff
Yassine <hondayassin5@gmail.com>
2024-07-31 21:34:23 +0100

    0 task

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs') 
 
-fs.writeFile(process.argv[1], process.argv[2], 'utf8', function (err) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
     if(err){
         console.log(err);
     }
