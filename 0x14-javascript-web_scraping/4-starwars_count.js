4b8e455eabd7854826d36657242fe3d190431af3
Yassine <hondayassin5@gmail.com>
2024-08-21 19:54:21 +0100

    0 task

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,23 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-const starWarsUrl = 'https://swapi-api.alx-tools.com/api/films/';
-
-request('https://swapi-api.alx-tools.com/api/films/', function (error, response, body) {
-  body =  JSON.parse(body).results;
-  for(let i = 0; i <body.length; i++){
-    
-    for(let i = 0; i <body.length; i++){
-        console.log(body[i].characters);
-       
+const url = process.argv[2];
+let count = 0;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const info = JSON.parse(body); // parse the body to JS object Notation
+    const info2 = info.results; // access to result information
+    for (let i = 0; i < info2.length; i++) {
+      if (info2[i].characters) { // if the characters information its available
+        for (const j of info2[i].characters) {
+          if (j.includes('18')) {
+            count++; // plus count if the ID its 18
+          }
+        }
       }
+    }
+    console.log(count);
   }
 });
